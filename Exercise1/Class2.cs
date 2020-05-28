dd3635ac3f81f4852ed5edac1a564d2624e64bb8
sandyy510 <61854748+sandyy510@users.noreply.github.com>
2020-05-28 18:06:51 -0500

    Commit All

---
+++ Exercise1/Class2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+
+namespace Exercise1
+{
+    public class Class2
+    {
+
+
+        public void ejercicio1()
+
+
+        {
+            int[] grades = { 6, 7, 10, 9, 8, 8, 5, 6 };
+
+            int[] grades2 = new int[8];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                grades2[i] = grades[i];
+            }
+
+            Console.WriteLine("\nFirst Five Values:\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("" + grades[i]);
+            }
+
+            int smallest = grades[0];
+            for (int i = 0; i < 8; i++)
+
+            {
+                if (grades[i] <= smallest)
+                {
+                    smallest = grades[i];
+
+                }
+            }
+
+            Console.WriteLine("\nThe smallest number is {0}\n\n", smallest);
+
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>();
+
+           
+            myDict["a"] = 2;
+            myDict["b"] = 7;
+            myDict["c"] = 5;
+
+         
+            foreach (var pair in myDict)
+            {
+                Console.WriteLine(pair.Key + "\n = " + pair.Value);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
+
+  
+
+ 
+
---
+++ Exercise1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena1 = "hola";
+            string cadena2 = "Hola";
+
+            if (cadena1.CompareTo(cadena2) == 0)
+                Console.WriteLine($"Both strings have same value.");
+            else
+                Console.WriteLine($"{cadena1} and {cadena2} are different.\n");
+
+
+            int[] numeros = { 10, 52, 5, 6, 69, 80, 23, 91 };
+
+            Console.WriteLine(numeros.Max());
+
+
+            string[] nombres = { "Jonathan", "Erick", "Susy", "Ana", "Pedro", "Veronica", "Lucas", "Karla", "Marcos", "Yanira", "Zuleima" };
+
+            int length = nombres.Length;
+
+            Array.Sort(nombres);
+
+            Console.WriteLine("\nArray ordenado:\n");
+
+            for (int i = 0; i < length; i++)
+            
+                Console.WriteLine(nombres[i]);
+            
+
+            string name = "Sandy";
+            int nameLength = name.Length;
+            
+                Console.WriteLine("\nThe name " + name + " contains " + nameLength + " letters.");
+            
+
+
+            Class2 ejercicio = new Class2();
+
+            ejercicio.ejercicio1();
+        
+
+        }
+    }
+
+}
+
